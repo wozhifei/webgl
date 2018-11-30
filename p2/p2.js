bdf6436de6d1db520817fe6204a8b0e4910d1a7c
wozhifei <wozhifei@live.com>
2018-11-30 15:48:19 +0900

    .

---
+++ p2/p2.js
@@ -0,0 +1,116 @@
+var VSHADER_SOURCE =
+  'attribute vec4 a_Position;\n' +
+  'attribute vec4 a_Color;\n' +
+  'uniform mat4 u_MvpMatrix;\n' +
+  'varying vec4 v_Color;\n' +
+  'void main() {\n' +
+  '  gl_Position = u_MvpMatrix * a_Position;\n' +
+  '  v_Color = a_Color;\n' +
+  '}\n';
+
+var FSHADER_SOURCE =
+  '#ifdef GL_ES\n' +
+  'precision mediump float;\n' +
+  '#endif\n' +
+  'varying vec4 v_Color;\n' +
+  'void main() {\n' +
+  '  gl_FragColor = v_Color;\n' +
+  '}\n';
+
+  function main(){
+    var canvas = document.getElementById("webgl");
+
+    var gl = getWebGLContext(canvas);
+    if(!gl){
+        console.log("Failed to get the rendering context for WebGL");
+        return;
+    }
+    
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to intialize shaders.');
+        return;
+    }
+
+    var n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('Failed to set the vertex information');
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    var u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
+    if (!u_MvpMatrix) {
+        console.log('Failed to get the storage location of u_MvpMatrix');
+        return;
+    }
+
+    var mvpMatrix = new Matrix4();
+    mvpMatrix.setPerspective(30, 1, 1, 100);
+    mvpMatrix.lookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);
+
+    gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
+  }
+
+function initVertexBuffers(gl) {
+
+  var vrtices = new Float32Array([
+    3,-0.5,-4,
+	-2,-2,2,
+	-2,-8,2,
+	4,3,2.2
+   ]);
+    
+   // Colors
+  var colors = new Float32Array([
+    1.0, 1.0, 1.0,    1.0, 1.0, 1.0,  1.0, 1.0, 1.0,   1.0, 1.0, 1.0
+   ]);
+    
+  // Indices of the vertices
+  var indices = new Uint8Array([0,1,2, 0,3,2]);
+
+  // Create a buffer object
+  var indexBuffer = gl.createBuffer();
+  if (!indexBuffer) 
+    return -1;
+
+  // Write the vertex coordinates and color to the buffer object
+  if (!initArrayBuffer(gl, vertices, 3, gl.FLOAT, 'a_Position'))
+    return -1;
+
+  if (!initArrayBuffer(gl, colors, 3, gl.FLOAT, 'a_Color'))
+    return -1;
+
+  // Write the indices to the buffer object
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+  return indices.length;
+}
+
+function initArrayBuffer(gl, data, num, type, attribute) {
+  var buffer = gl.createBuffer();   // Create a buffer object
+  if (!buffer) {
+    console.log('Failed to create the buffer object');
+    return false;
+  }
+  // Write date into the buffer object
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+  // Assign the buffer object to the attribute variable
+  var a_attribute = gl.getAttribLocation(gl.program, attribute);
+  if (a_attribute < 0) {
+    console.log('Failed to get the storage location of ' + attribute);
+    return false;
+  }
+  gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
+  // Enable the assignment of the buffer object to the attribute variable
+  gl.enableVertexAttribArray(a_attribute);
+
+  return true;
+}
